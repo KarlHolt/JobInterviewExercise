05376d269f43a844349c5569c1203ac85897db2b
KarlHolt <karlimmanuelholt@tutanota.com>
2023-11-25 21:12:23 +0100

    getting started on the product

--- app.ts
+++ app.ts
@@ -1,30 +1,49 @@
 const express = require('express');
 import * as fs from 'fs';
-const Configuration = require('openai');
-const OpenAIApi = require('openai');
+import OpenAI from 'openai';
 
+// The sad realasation has come to me, that the typescript library I'm using does not
+// seem to have support for assistant, and therefore I can not have it renember outputs
+// and inputs. I will do that manually.
+
+
+// Load keys
 let rawData = fs.readFileSync('keys.json', 'utf8');
-let jsonData = JSON.parse(rawData);
+let keys = JSON.parse(rawData);
+
+// Initialize OpenAI
+const openai = new OpenAI({
+	organization: keys["organization"],
+	apiKey: keys["openai"],
+});
+
+// Server set up
 const app = express();
 const port = 3000;
 
-const configuration = new Configuration({
-	organization: jsonData["organization"],
-	apiKey: jsonData["openai"],
-});
-
-async function hehe(){
-	const openai = new OpenAIApi(configuration);
-	const response = await openai.listEngines();
-	return response
-}
 
 
-app.get('/', (req: any, res: any) => {
-	res.send(hehe());
-  	res.sendFile("/home/fony/Git/small_project/home.html");
+
+app.get('/', async (req: any, res: any) => {
+	res.send(req)
+	//res.send(await main());
+  	res.sendFile("/home/fony/Git/JobInterviewExercise/home.html");
 });
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+
+// async function main() {
+// 	const params: OpenAI.Chat.ChatCompletionCreateParams = {
+// 	  messages: [{ role: 'user', content: 'Say this is a test' }],
+// 	  model: 'gpt-3.5-turbo',
+// 	};  
+// 	const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+// 	return chatCompletion;
+// }
+
+// main().then((value) => {
+// 	console.log(value);
+// })
